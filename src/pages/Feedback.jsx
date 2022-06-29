e7e2e4ca1e9bab60312f17f053188b9bc2d6c5ee
Arthur Coelho <arthurcoelho7885@gmail.com>
2022-06-29 16:19:14 -0300

    requisito 14 e 15

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
 
@@ -7,14 +8,18 @@ const NUMBER_TRES = 3;
 
 class Feedback extends Component {
   render() {
-    const { acertos } = this.props;
+    const { acertos, score } = this.props;
     return (
       <div>
         <Header />
         <p data-testid="feedback-text">
           { acertos < NUMBER_TRES ? 'Could be better...' : 'Well Done!' }
         </p>
-        {/* <p data-testid="feedback-total-question">{ score }</p> */}
+        <p data-testid="feedback-total-question">{ acertos }</p>
+        <p data-testid="feedback-total-score">{ score }</p>
+        <Link to="/">
+          <button data-testid="btn-play-again" type="button">Play Again</button>
+        </Link>
       </div>
     );
   }
@@ -22,10 +27,12 @@ class Feedback extends Component {
 
 const mapStateToProps = (state) => ({
   acertos: state.player.assertions,
+  score: state.player.score,
 });
 
 Feedback.propTypes = {
   acertos: PropTypes.number.isRequired,
+  score: PropTypes.number.isRequired,
 };
 
 export default connect(mapStateToProps)(Feedback);
